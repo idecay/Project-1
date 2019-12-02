83d1c4fb28a739935d1c69888c30f7d24b8e2e98
idecay <greenwood.cm88@gmail.com>
2019-12-02 15:28:47 -0500

    modify layout

--- script.js
+++ script.js
@@ -4,3 +4,37 @@ Swal.fire({
     icon: 'question',
     confirmButtonText: 'Lets go!'
   })
+
+
+
+
+
+
+
+
+
+
+// let timerInterval
+// Swal.fire({
+//   title: 'Auto close alert!',
+//   html: 'I will close in <b></b> milliseconds.',
+//   timer: 15000,
+//   timerProgressBar: true,
+//   onBeforeOpen: () => {
+//     Swal.showLoading()
+//     timerInterval = setInterval(() => {
+//       Swal.getContent().querySelector('b')
+//         .textContent = Swal.getTimerLeft()
+//     }, 100)
+//   },
+//   onClose: () => {
+//     clearInterval(timerInterval)
+//   }
+// }).then((result) => {
+//   if (
+//     /* Read more about handling dismissals below */
+//     result.dismiss === Swal.DismissReason.timer
+//   ) {
+//     console.log('I was closed by the timer') // eslint-disable-line
+//   }
+// })
