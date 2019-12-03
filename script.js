5072e2f98f0d3d8cea474f1776bea0e784aed26b
idecay <greenwood.cm88@gmail.com>
2019-12-03 02:04:39 -0500

    remove sweetalert

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-Swal.fire({
-    title: 'Welcome!',
-    text: 'Do you want to play Jeopardy?',
-    icon: 'question',
-    confirmButtonText: 'Lets go!'
-  })
+// Swal.fire({
+//     title: 'Welcome!',
+//     text: 'Do you want to play Jeopardy?',
+//     icon: 'question',
+//     confirmButtonText: 'Lets go!'
+//   })
 
 
 //create game board
 
 //for each category
+let container = document.createElement('div');
+document.body.appendChild(container).classList.add("container");
 for(let i = 0; i < 6; i++) {
-    //name category
     let column = document.createElement('div');
     document.body.appendChild(column).classList.add("btn-group-vertical");
     console.log("cat" + i);
@@ -19,15 +20,12 @@ for(let i = 0; i < 6; i++) {
         //create button with value of 200 x question
         let tile = document.createElement('button');
         let columnSelector = document.querySelectorAll('.btn-group-vertical');
-
         tile.type = "button";
         tile.className = "btn btn-primary"
         tile.dataset.toggle = "button"
-        //tile.aria-pressed = "false"
-       // <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false">200</button>
-
-
+        
         if (j === 0) {
+            //name category
             columnSelector[i].appendChild(tile).innerText = "Cat" + i;
         } else {
             columnSelector[i].appendChild(tile).innerText = j * 200;
